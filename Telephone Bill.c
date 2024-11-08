879faf77411f6e034b7bf3293cea5cd2c29c8238
MUDIT CHAUHAN <93607579+Mudi13@users.noreply.github.com>
2024-11-08 20:54:12 +0530

    Add files via upload

---
+++ Telephone Bill.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    float call,bill;
+    scanf("%f",&call);
+
+    if ((call>0) && (call<=50))
+    {
+        bill=100;
+    }
+
+    else if ((call>50) && (call<=100))
+    {
+        bill=100+(0.80*(call-50));
+    }
+
+    else if ((call>100) && (call<=200))
+    {
+        bill=100 + (0.80*(50)) + (0.60*(call-100));
+    }
+
+    else if (call>200)
+    {
+        bill=100 + (0.80*(50)) + (0.60*(100)) + (0.40*(call-200));
+    }
+
+    printf("Bill= %f",bill);
+
+
+
+
+}
